748e23743e4073412f9f727d5f472e59cdf4309a
rommel-exe <141284780+rommel-exe@users.noreply.github.com>
2026-04-26 19:04:55 +1000

    Fix startup auto-update trigger in 0.0.3

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import { runLaunchAutoUpdate } from "./features/updates/autoUpdate";
 import "./index.css";
+
+void runLaunchAutoUpdate();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
